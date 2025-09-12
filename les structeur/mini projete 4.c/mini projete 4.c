4f8e27dd52c8aabbd1e5e349bb72bf830f074982
Saadia20256 <haddachsaadia089@gmail.com>
2025-09-12 12:35:47 +0200

    mini projete 4.c

---
+++ les structeur/mini projete 4.c/mini projete 4.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<string.h>
+
+typedef max des etudaints ;
+typedef max des cours ;
+
+struct etudaints 
+{
+    int id ;
+    char nom[20];
+    char prenom[20];
+    float moyenne ;
+    int age; 
+}
+struct cours 
+{
+   char code [10];
+   char nom [100];
+   int credits ;
+   float note ; 
+}
+struct etudaint [max_etudaint]
+struct cours [max_etudaint]
+
+int nb_etudaint =0;
+int nb_cour =0 ;
+
+void ajoutre des etudaints 
+{
+    if(nb_etudaint<max_etudaint)}
+
+    struct etudaint i
+    printf("entrer l'id de  etudaint :");
+    scanf("%d",&id);
+    printf("entrer le nom de  etudaint");
+    scanf("%s",&nom);
+    printf("entrer le prenom de etudaint :");
+    scanf("%s",&prenom);
+    printf("entrerla moyenne de etudaint :");
+    scanf("%f",&moyenne);
+    printf("entrer age de etudaint :");
+    scanf("%d",&age);
+}elese {
+    printf("limit des etudaints :\n");
+} 
+void afficher_etudiants() {
+    
+        printf("Liste des étudiants :\n");
+        for (int i = 0; i < nb_etudiants; i++) {
+            printf("ID: %d, Nom: %s, Prénom: %s, Âge: %d, Moyenne: %.2f\n",
+                   etudiants[i].id, etudiants[i].nom, etudiants[i].prenom,
+                   etudiants[i].age, etudiants[i].moyenne);
+        }
+    }
+
+struct cours 
+printf("entrer")
+
+
